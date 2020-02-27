9199e8a9a9ff771d6d37b56fe030ff8511577be2
Sohan <sohan.patil98@gmail.com>
2020-02-27 19:08:29 +0530

    flask test

--- web/Flask_env/Scripts/activate.bat
+++ web/Flask_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\bmah0\Documents\GitHub\nayamandi\web\Flask_env
+set VIRTUAL_ENV=C:\Users\Sohan\Desktop\nayamandi\web\Flask_env
 
 if not defined PROMPT set PROMPT=$P$G
 
